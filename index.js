f3e9c1f429b44aaa7af651e8faa2454a437e56e7
Alejandro Oropeza <Aoropez>
2019-12-19 17:12:51 -0600

    It's working but the approach it's wrong ᕦ╏^^╏ᕤ

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 "use strict";
 
-var bluebird = require("bluebird");
+const bluebird = require("bluebird");
 const puppeteer = require("puppeteer");
+const { flattenDepth, uniqBy } = require("lodash");
+
+const optionsGoTo = {
+  timeout: 0,
+  waitUntil: "domcontentloaded"
+};
 
 const delay = time =>
   new Promise(resolve => {
     setTimeout(resolve, time);
   });
 
-const waitToElementDisappear = async (elementId, time, _page) => {
-  let loading = await _page.$$(elementId);
+const waitForElements = async (selector, time, _page) => {
+  let loading = await _page.$$(selector);
 
   if (loading.length > 0) {
-    console.log(
-      `element ${elementId} still exists, testing again in ${time} ms`
-    );
+    console.log(`many element for ${selector}, finishing`);
+
+    console.log("loading");
+    console.log(loading);
 
     await delay(time);
-    await waitToElementDisappear(elementId, time, _page);
+    return;
   }
+  console.log(`zero elements for ${selector}, testing again in ${time} ms`);
+  await delay(time);
+  await waitForElements(selector, time, _page);
 };
 
-const getMoviesName = async (city, _page) => {
+const scrapeMoviesInfo = async (city, _page) => {
   // select cities
   await _page.select("#cmbCiudadesCartelera", city);
 
-  await waitToElementDisappear("#preloadCartelera", 200, _page);
+  await waitForElements("ul.listCartelera>li", 200, _page);
 
   return _page.evaluate(() => {
     var allItemList = Array.from(
@@ -35,8 +45,30 @@ const getMoviesName = async (city, _page) => {
     var moviesNames = [];
     allItemList.forEach(movie => {
       var h1Title = movie.querySelector("h1");
+      var anchorCover = movie.querySelector("figure");
+
+      var anchorSchedules = null;
+      if (movie.querySelector(".btn-call")) {
+        var anchorsActions = Array.from(
+          movie.querySelector(".btn-call").querySelectorAll("a")
+        );
+
+        anchorsActions.forEach(anchor => {
+          var anchorClass = anchor.getAttribute("class");
+          if (typeof anchorClass != "string") {
+            anchorSchedules = anchor.getAttribute("href");
+          }
+        });
+      }
+
       if (h1Title) {
-        moviesNames.push(h1Title.innerText);
+        moviesNames.push({
+          name: h1Title.innerText,
+          cover: anchorCover
+            ? anchorCover.querySelector("img").getAttribute("src")
+            : null,
+          anchorSchedules
+        });
       }
     });
     return moviesNames;
@@ -45,36 +77,52 @@ const getMoviesName = async (city, _page) => {
 
 (async () => {
   try {
+    const baseUrlBrand = "https://cinepolis.com/";
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.setViewport({ width: 1000, height: 800 });
-    await page.goto("https://cinepolis.com/", {
-      timeout: 0,
-      waitUntil: "domcontentloaded"
-    });
-
-    // 20 CDMX Centro
-    // 21 CDMX Norte
-    // 22 CDMX Oriente
-    // 23 CDMX Poniente
-    // 24 CDMX Sur
+    await page.goto(baseUrlBrand, optionsGoTo);
 
     const cities = [
-      { "20": "CDMX Centro" },
-      { "21": "CDMX Norte" },
-      { "22": "CDMX Oriente" },
-      { "23": "CDMX Poniente" },
-      { "24": "CDMX Sur" }
+      {
+        id: "20",
+        key: "cdmx-centro"
+      } /*,
+      { id: "21", key: "cdmx-norte" },
+      { id: "22", key: "cdmx-oriente" },
+      { id: "23", key: "cdmx-poniente" },
+      { id: "24", key: "cdmx-sur" }*/
     ];
 
-    const result = await bluebird.mapSeries(cities, item =>
-      getMoviesName(Object.keys(item)[0], page)
+    const arrayResults = await bluebird.mapSeries(cities, item =>
+      scrapeMoviesInfo(item.id, page)
     );
+    const resultUnique = uniqBy(flattenDepth(arrayResults), "name");
 
-    console.log("moviesNames", result);
+    console.log("Finishing getting movies for all cities:", resultUnique);
+
+    const urlSchedules = `${baseUrlBrand}${resultUnique[0].anchorSchedules}`;
+
+    /*
+    console.log(`urlSchedules: ${urlSchedules}`);
+    await page.goto(urlSchedules, optionsGoTo);
+
+    await waitForElements("ul.chosen-choices>li.search-choice", 200, page);
+
+    delay(10000);
+    const result = await page.evaluate(() => {
+      var allItemList = Array.from(
+        document.querySelectorAll("ul.chosen-choices>li")
+      );
+      return allItemList.length;
+    });
+
+    console.log(`result locations: ${result}`);
+    */
 
     await browser.close();
   } catch (e) {
     console.log(e);
   }
 })();
+// bgpreload_Gris ng-hide
